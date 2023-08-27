ba0480fb5933971b3c9a63ea78d04c8e0d55a166
Anil Kushwaha <113308156+Anil-Maurya-2002@users.noreply.github.com>
2023-08-27 20:33:42 +0530

    Add files via upload

---
+++ OOPS.53.PY.py
@@ -0,0 +1,23 @@
+# 53 Classes & Object (OOPS)
+   # Classes - Template
+   # Object _ Instance of the class
+
+# 54 Creating a class
+class student:
+    pass
+harry = student ()
+larry = student ()
+
+harry.name = "Harry"
+harry.std = 12
+harry.sec = "A"
+
+larry.name = "larry"
+larry.std = "b.tech"
+larry.sec = "C"
+
+print("Name = ", harry.name,",std =" ,harry.std,",section=",harry.sec)
+print(larry.name,larry.std,larry.sec)
+
+
+# print(student.__dict__)
