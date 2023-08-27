7c28951dcfbd1573b642ac2444bb88c047b8dcff
Anil Kushwaha <113308156+Anil-Maurya-2002@users.noreply.github.com>
2023-08-27 20:38:07 +0530

    Add files via upload

---
+++ Add,Mul,Calcuter.4.py
@@ -0,0 +1,14 @@
+# add,mul
+a=4
+b=5
+c=a+b
+print(c*c)
+
+# calcuter
+print("enter 1 st no")
+n1=input()
+print("enter 2nd no")
+n2=input()
+print("sum of two no is",int(n1)+int(n2))
+print("differant of two no is",int(n1)-int(n2))
+
