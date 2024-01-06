7b83bafea7b6cc630653f3f4af942b1338868e63
Anil Kushwaha <113308156+Anil-Maurya-2002@users.noreply.github.com>
2024-01-06 23:25:37 +0530

    Add files via upload

---
+++ If Else & Elif.14.py
@@ -0,0 +1,36 @@
+# 14 If Else & Elif
+a=6
+b=56
+print("Enter the no.")
+c=int(input())
+    print("greater")
+elif c==b:
+if c>b:
+    print("Equal")
+else:
+    print("Lesser \n ")
+
+# in & not in
+list=[5,7,3]
+print(15 in list)
+if 15 not in list:
+    print("No not in list \n ")
+    print(5 in list)
+    if 5 in list:
+        print("Yes in list \n ")
+
+# Quiz
+print("what is your age")
+age=int(input())
+if age>18:
+    print("you can drive care")
+elif age==18:
+    print("come in senter for vearyfy")
+else:print("Not drive care\n  ")
+
+# Quiz
+print("what is your age")
+age=int(input())
+if age >= 5:
+    print("You can use computer")
+else:print("can not use computer ")
